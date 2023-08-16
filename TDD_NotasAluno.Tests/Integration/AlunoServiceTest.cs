c0759a0e35cb1a5b6bdfb95711c6221587bd4371
Lucas Gonçalves <lucas.goncalves@forpeople.io>
2023-08-16 14:45:50 -0300

    IntegrationTests refatorados, para eliminar casos de testes já realizdos nos UnitTests

--- TDD_NotasAluno.Tests/Integration/AlunoServiceTest.cs
+++ TDD_NotasAluno.Tests/Integration/AlunoServiceTest.cs
@@ -1,5 +1,8 @@
 ﻿namespace TDD_NotasAluno.Tests.Integration
 {
+    //Não é necessário testar todos os casos do teste unitário, pois o comportamento interno
+    //já foi testado.
+    //Este deve focar realmente na integração dos recursos às lógicas de domínio
     public class AlunoServiceTest
     {
         //Broad Integration Test
@@ -20,52 +23,6 @@
 
             float media = (float)result.GetType().GetProperty("media")!.GetValue(result)!;
             Assert.Equal(9, media);
-
-            var notas = (List<Nota>)result.GetType().GetProperty("notas")!.GetValue(result)!;
-            Assert.Equal(3, notas.Count);
-        }
-
-        //Narrow Integration Test
-        [Fact]
-        public async void CalcularMediaSimplesAlunoMemory()
-        {
-            //DADO
-            int idAluno = 1;
-            IAlunoData alunoData = new AlunoDataMemory();
-            INotaData notaData = new NotaDataMemory();
-            AlunoService alunoService = new(alunoData, notaData);
-
-            //QUANDO
-            await alunoService.CalcularMediaAsync(idAluno, "simples");
-
-            //ENTÃO
-            var result = await alunoService.GetMediaAlunoByIdAsync(idAluno);
-
-            float media = (float)result.GetType().GetProperty("media")!.GetValue(result)!;
-            Assert.Equal(9, media);
-
-            var notas = (List<Nota>)result.GetType().GetProperty("notas")!.GetValue(result)!;
-            Assert.Equal(3, notas.Count);
-        }
-
-        //Broad Integration Test
-        [Fact]
-        public async void CalcularMediaPonderadaAlunoSqlServer()
-        {
-            //DADO
-            int idAluno = 1;
-            IAlunoData alunoData = new AlunoDataSqlServer(new EfSqlServerAdapter());
-            INotaData notaData = new NotaDataSqlServer(new EfSqlServerAdapter());
-            AlunoService alunoService = new(alunoData, notaData);
-
-            //QUANDO
-            await alunoService.CalcularMediaAsync(idAluno, "ponderada");
-
-            //ENTÃO
-            var result = await alunoService.GetMediaAlunoByIdAsync(idAluno);
-
-            float media = (float)result.GetType().GetProperty("media")!.GetValue(result)!;
-            Assert.Equal(8.9, Math.Round(media, 2));
 
             var notas = (List<Nota>)result.GetType().GetProperty("notas")!.GetValue(result)!;
             Assert.Equal(3, notas.Count);
--- TDD_NotasAluno.Tests/Integration/ApiTest.cs
+++ TDD_NotasAluno.Tests/Integration/ApiTest.cs
@@ -1,32 +1,11 @@
 namespace TDD_NotasAluno.Tests.Integration
 {
+    //Não é necessário testar todos os casos do teste unitário, pois o comportamento interno
+    //já foi testado.
+    //Este deve focar realmente na integração dos recursos às lógicas de domínio
     public class ApiTest
     {
         static HttpClient HttpClient = new() { BaseAddress = new Uri("http://localhost:5033") };
-
-        [Fact]
-        public async void CalcularMediaSimplesDeAluno()
-        {
-            //TEST_FIRST:
-            //Dado
-            int idAluno = 1;
-            string tipoCalculoMedia = "simples";
-
-            await HttpClient.PostAsJsonAsync($"/alunos/{idAluno}/calculaMedia", tipoCalculoMedia);
-
-            //Quando
-            var response = await HttpClient.GetAsync($"/alunos/{idAluno}");
-
-            //Então
-            var output = await response.Content.ReadAsStringAsync();
-            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(output)!;
-
-            float media = (float)obj.media;
-            Assert.Equal(9, media);
-
-            var notas = obj.notas;
-            Assert.Equal(3, notas.Count);
-        }
 
         [Fact]
         public async void CalcularMediaPonderadaDeAluno()
